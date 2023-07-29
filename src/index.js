3c0f264e0793c5ddccc7aa69c69ba42a9a8c50d0
Visanu <mvisanu@gmail.com>
2023-07-29 08:52:55 -0500

    add functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function MainHeader() {
+  return <h1>React Course</h1>;
+}
+
+function SubHeader() {
+  return <h2>This will be an exciting course</h2>;
+}
+
+function Header() {
+  return(
+    <div>
+    <MainHeader/>
+    <SubHeader/>
+    </div>
+    
+  )
+}
+
+function Footer() {
+  return <p style={{color:"gray",backgroundColor:"black"}}>Happy coding</p>;
+}
+function MainBody() {
+  return (
+    <div>
+      <p>Learn React</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to Walmart</li>
+      </ul>
+    </div>
+  );
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <MainBody />
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
